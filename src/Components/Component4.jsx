2b203c30a26c3ac1358b23eb9bc6fdb7b8197bf9
sonamza <sonamzangmo1002@gmail.com>
2025-04-22 02:21:07 -0700

    first commit

---
+++ src/Components/Component4.jsx
@@ -0,0 +1,12 @@
+import Component5 from "./component5"
+
+const Component4 = () => {
+    return (
+      <>
+        <h1>Component 4</h1>
+        <Component5/>
+      </>
+    )
+  }
+
+  export default Component4
---
+++ src/hook/AppContext.js
@@ -0,0 +1,7 @@
+// UserContext.js
+import { createContext } from "react"
+
+// Create the context
+const AppContext = createContext()
+
+export default AppContext
